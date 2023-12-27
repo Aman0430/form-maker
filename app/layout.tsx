a5459772d9c73dda3c76331d6f87edbe79b1e0c3
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2023-12-27 21:04:33 +0530

    feat: prisma setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ThemeProvider
-            attribute="clas"
+            attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
