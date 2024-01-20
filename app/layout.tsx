f030b3b4c1518dbf7266f791da9ba88a5320a859
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2024-01-20 16:36:54 +0530

    fix: builder bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PageForm (@CodeWithKliton)",
-  description: "Check my youtube channel for more content",
+  title: "Form builder",
+  description: "A Web App to create Forms",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,11 +3,8 @@ import React from "react";
 
 function Logo() {
   return (
-    <Link
-      href={"/"}
-      className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer"
-    >
-      PageForm
+    <Link href={"/"} className="font-bold text-3xl bg-red-600 text-transparent bg-clip-text hover:cursor-pointer">
+      FormBuilder
     </Link>
   );
 }
